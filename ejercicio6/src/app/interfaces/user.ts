ce09c89474dec60f91e804ee0e2db62bad3e08e3
Andrea <andreamania10@gmail.com>
2026-05-03 13:05:24 +0200

    interface users Api Response

--- ejercicio6/src/app/interfaces/user.ts
+++ ejercicio6/src/app/interfaces/user.ts
@@ -1,1 +1,19 @@
-export interface User {}
+export interface User {
+    _id: string,
+    name: string,
+    lastName: string,
+    email: string,
+    image: string,
+    age: number
+}
+
+export interface ApiResponse {
+    ok: boolean,
+    result: User[],
+    total?: number
+}
+
+export interface ApiResponseSingle {
+    ok: boolean,
+    result: User
+}
